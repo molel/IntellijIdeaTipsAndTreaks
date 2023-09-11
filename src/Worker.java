6ce91886e494d003b2dcc433361bf3a283b245e1
molel <kapitov.04@gmail.com>
2023-09-11 15:30:46 +0300

    Updated worker class

--- src/Worker.java
+++ src/Worker.java
@@ -1,21 +1,25 @@
 public class Worker extends Person {
-    private int salary;
+    private float salary;
 
-    public int getSalary() {
+    public float getSalary() {
         return salary;
     }
 
-    public void setSalary(int salary) {
+    public void setSalary(float salary) {
         this.salary = salary;
     }
 
-    public Worker(String name, int age, float weight, float height, int salary) {
+    public Worker(String name, int age, float weight, float height, float salary) {
         super(name, age, weight, height);
         this.salary = salary;
     }
 
     public void doWork() {
-        System.out.println("I'm working...");
+        System.out.println("I'm working... but on new project");
+    }
+
+    public void doBreak() {
+        System.out.println("Don't touch me, I'm eating my sandwich");
     }
 
 }
